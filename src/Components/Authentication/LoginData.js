b81c823e5413dcba726e3824730a39e3fd4a0d88
Haris Ali <haris.ali@empglabs.com>
2022-07-26 11:31:41 +0500

    Added Table in /user route

--- src/Components/Authentication/LoginData.js
+++ src/Components/Authentication/LoginData.js
@@ -1,12 +1,34 @@
 import ListViewer from "../Shared/ListViewer";
 import useFetch from "../Hooks/Fetch";
+import { Table } from "react-bootstrap";
 
 const LoginData = () => {
   const [jsonData] = useFetch("https://jsonplaceholder.typicode.com/users");
 
   return (
     <div>
-      {JSON.stringify(jsonData)}
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Sr. #</th>
+            <th>Name</th>
+            <th>Username</th>
+            <th>E-Mail</th>
+          </tr>
+        </thead>
+        {jsonData.map((user) => {
+          return (
+            <tbody>
+              <tr>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+              </tr>
+            </tbody>
+          );
+        })}
+      </Table>
       {console.log(localStorage.getItem("isAuthenticated"))}
     </div>
   );
